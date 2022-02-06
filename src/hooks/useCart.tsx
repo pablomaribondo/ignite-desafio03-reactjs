3ea2208568eeb48d3d9a3c64f4f70a61b6cae173
Pablo Maribondo <pablomgvieira@gmail.com>
2022-02-06 11:37:36 -0300

    feat: create addProduct feature

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -34,9 +34,37 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const { data: stockData } = await api.get(`/stock/${productId}`);
+
+      if (stockData.amount === 0) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      const productIndex = cart.findIndex(product => product.id === productId);
+      let updatedCart: Product[];
+
+      if (productIndex === -1) {
+        const { data: productData } = await api.get(`/products/${productId}`);
+        const product = { ...productData, amount: 1 };
+        updatedCart = [...cart, product];
+      } else {
+        if (stockData.amount < cart[productIndex].amount + 1) {
+          toast.error("Quantidade solicitada fora de estoque");
+          return;
+        }
+
+        updatedCart = [...cart];
+        updatedCart[productIndex] = {
+          ...updatedCart[productIndex],
+          amount: updatedCart[productIndex].amount + 1,
+        };
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart));
+    } catch (error) {
+      toast.error("Erro na adição do produto");
     }
   };
 
